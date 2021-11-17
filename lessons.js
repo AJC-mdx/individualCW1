54a7904e3fc4f5ab9d21f884de39ce78a29c4b67
Ahmed Choudhry <AC1953@live.mdx.ac.uk>
2021-11-17 22:27:46 +0000

    added checkout form and 'place order' button

--- lessons.js
+++ lessons.js
@@ -1,4 +1,3 @@
-let n = 5;
 let Lesson = new Vue({
     el: '#app',
     data: {
@@ -8,20 +7,39 @@ let Lesson = new Vue({
             Subject: 'Maths',
             Location: 'London',
             Price: '$100',
-            Spaces: n,
+            Spaces: 5,
             Image: 'https://img.icons8.com/ios/50/000000/math.png'
         },
-        cart:[]
+        cart:[],
+        order: {
+            firstName:'',
+            lastName:'',
+            phoneNumber:''
+        }
     },
  methods: {
  checkResponse(){
-        this.Lesson.Spaces= -1 + n--;
+    this.reduceSpace();
+    this.addToCart();
+ },
+ reduceSpace(){
+        this.Lesson.Spaces = this.Lesson.Spaces - 1;
+ },
+ addToCart(){
+        this.cart.push(this.Lesson.Subject);
  },
    showCheckout(){
        this.showLesson = this.showLesson ? false : true
+   },
+
+   confirmOrder(){
+       alert('Your order has been submitted.')
    }
  },
  computed: {
+     cartLength() {
+         return this.cart.length
+     },
      canAddToCart() {
          return this.Lesson.Spaces > 0
      }
